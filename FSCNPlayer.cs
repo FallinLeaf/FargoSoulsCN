5d48794cad1edf9174090ab9b044499979496fbb
Sky-Meteor <2746501855@qq.com>
2022-09-17 18:53:20 +0800

    quite a number of translations

--- FSCNPlayer.cs
+++ FSCNPlayer.cs
@@ -1,48 +1,18 @@
-﻿/*using FargowiltasSouls;
+﻿using FargowiltasSouls;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using FargowiltasSouls.Buffs.Masomode;
 
 namespace FargoSoulsCN
 {
     public class FSCNPlayer : ModPlayer
     {
-        public override bool PreKill(double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
+        public override void UpdateDead()
         {
-            FargoSoulsPlayer fargoSoulsPlayer = Player.GetModPlayer<FargoSoulsPlayer>();
-            bool MatchDamageSource(string source)
-            {
-                var name = fargoSoulsPlayer.GetType().GetField(source);
-                bool value = (bool)name.GetValue(fargoSoulsPlayer);
-                if (value)
-                    return true;
-                return false;
-            }
-            PlayerDeathReason DeathByLocalization(string key)
-            {
-                string death = Language.GetTextValue($"Mods.FargowiltasSouls.DeathMessage.{key}");
-                return PlayerDeathReason.ByCustomReason($"{Player.name}{death}");
-            }
-            bool dot = damage == 10.0 && hitDirection == 0;
-            if ((MatchDamageSource("GodEater") || MatchDamageSource("FlamesoftheUniverse") || MatchDamageSource("CurseoftheMoon") || MatchDamageSource("MutantFang")) && dot)
-                damageSource = DeathByLocalization("DivineWrath");
-
-            if (MatchDamageSource("Infested") && dot)
-                damageSource = DeathByLocalization("Infested");
-
-            if (MatchDamageSource("Anticoagulation") && dot)
-                damageSource = DeathByLocalization("Anticoagulation");
-
-            if (MatchDamageSource("Rotting") && dot)
-                damageSource = DeathByLocalization("Rotting");
-
-            if (MatchDamageSource("Shadowflame") && dot)
-                damageSource = DeathByLocalization("Shadowflame");
-
-            if (MatchDamageSource("NanoInjection") && dot)
-                damageSource = DeathByLocalization("NanoInjection");
-            return true;
+            if (Player.respawnTimer > 0)
+                Player.respawnTimer -= 6;
         }
     }
-}*/
+}
--- MiscTranslate.cs
+++ MiscTranslate.cs
@@ -18,28 +18,68 @@ using Terraria.ModLoader;
 using Terraria.Localization;
 using Microsoft.Xna.Framework.Input;
 using FargowiltasSouls.EternityMode.Content.Enemy.Jungle;
+using MonoMod.Utils;
+using FargowiltasSouls.UI;
+using Microsoft.Xna.Framework.Graphics;
+using FargowiltasSouls.Projectiles.Masomode;
+using FargowiltasSouls.NPCs;
+using FargowiltasSouls.NPCs.DeviBoss;
+using FargowiltasSouls.EternityMode.Content.Boss.HM;
 
 namespace FargoSoulsCN
 {
-    public class DeathMessageTranslate : ModSystem
+    public class MiscTranslate : ModSystem
     {
         public List<ILHook> ilHooks;
         public List<Hook> hooks;
         public override void Load()
         {
+            On.Terraria.Main.DrawInterface_33_MouseText += Main_DrawInterface_33_MouseText;
+            On.Terraria.UI.Chat.ChatManager.ParseMessage += ChatManager_ParseMessage;
             hooks = new List<Hook>();
             ilHooks = new List<ILHook>();
             MonoModHooks.RequestNativeAccess();
             hooks.Add(new Hook(typeof(FargoSoulsPlayer).GetMethod("PreKill"), PreKill));
-            ilHooks.Add(new ILHook(typeof(Derpling).GetMethod("AI"), new ILContext.Manipulator(il =>
+            hooks.Add(new Hook(typeof(Fused).GetMethod("Update", new Type[] { typeof(Player), typeof(int).MakeByRefType() }), Update));
+            hooks.Add(new Hook(typeof(FakeHeart).GetMethod("CanHitPlayer"), CanHitPlayer));
+            void ILTranslate(Type type, string methodName, string en, string zh, Type[] types, BindingFlags flags = BindingFlags.Public | BindingFlags.Instance)
             {
-                var c = new ILCursor(il);
-                if (!c.TryGotoNext(i => i.MatchLdstr(" was sucked dry.")))
-                    return;
-                c.Index++;
-                c.Emit(OpCodes.Pop);
-                c.Emit(OpCodes.Ldstr, "被吸干了。");
-            })));
+                ilHooks.Add(new ILHook(type.GetMethod(methodName, flags, types), new ILContext.Manipulator(il =>
+                {
+                    var c = new ILCursor(il);
+                    if (!c.TryGotoNext(i => i.MatchLdstr(en)))
+                        return;
+                    c.Index++;
+                    c.Emit(OpCodes.Pop);
+                    c.Emit(OpCodes.Ldstr, zh);
+                })));
+            }
+            ILTranslate(typeof(Derpling), "AI", " was sucked dry.", "被吸干了。", new Type[] { typeof(NPC) });
+            ILTranslate(typeof(UISearchBar), "DrawChildren", "Search...", "搜索……", new Type[] { typeof(SpriteBatch) }, BindingFlags.NonPublic | BindingFlags.Instance);
+            #region Betsy
+            void BetsyTranslate(string en, string zh)
+            {
+                ilHooks.Add(new ILHook(typeof(Betsy).GetMethod("PreAI"), new ILContext.Manipulator(il =>
+                {
+                    var c = new ILCursor(il);
+                    if (!c.TryGotoNext(i => i.MatchLdstr(en)))
+                        return;
+                    c.Index++;
+                    c.Emit(OpCodes.Pop);
+                    c.Emit(OpCodes.Ldstr, zh);
+                })));
+            }
+            BetsyTranslate("CRINGE", "你怕了？");
+            #endregion
+            foreach (MethodInfo info in typeof(Fused).GetMethods())
+            {
+
+                if (info.Name == "Update")
+                {
+                    Mod.Logger.Warn(info.GetParameters());
+
+                }
+            }
             foreach (ILHook iLHook in ilHooks)
             {
                 if (iLHook is not null)
@@ -51,6 +91,22 @@ namespace FargoSoulsCN
                     hook.Apply();
             }
         }
+
+        private List<Terraria.UI.Chat.TextSnippet> ChatManager_ParseMessage(On.Terraria.UI.Chat.ChatManager.orig_ParseMessage orig, string text, Color baseColor)
+        {
+            if (text == "Eternity Mode is enabled")
+                text = "永恒模式已开启";
+            if (text == "Masochist Mode is enabled")
+                text = "受虐模式已开启";
+            return orig.Invoke(text, baseColor);
+        }
+        private static void Main_DrawInterface_33_MouseText(On.Terraria.Main.orig_DrawInterface_33_MouseText orig, Main self)
+        {
+            if (Main.hoverItemName == "Configure Accessory Effects")
+                Main.hoverItemName = "设置饰品效果";
+            orig.Invoke(self);
+        }
+
         private static bool PreKill(FargoSoulsPlayer orig, double damage, int hitDirection, bool pvp, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
         {
             bool retVal = true;
@@ -149,8 +205,52 @@ namespace FargoSoulsCN
 
             return retVal;
         }
+        private static void Update(Fused orig, Player player, ref int buffIndex)
+        {
+            player.GetModPlayer<FargoSoulsPlayer>().Fused = true;
+
+            if (player.buffTime[buffIndex] == 2)
+            {
+                player.immune = false;
+                player.immuneTime = 0;
+                int damage = (int)(Math.Max(player.statLife, player.statLifeMax) * 2.0 / 3.0);
+                player.Hurt(PlayerDeathReason.ByCustomReason(player.name + "被炸成了碎片。"), damage, 0, false, false, true);
+                Projectile.NewProjectile(player.GetSource_Buff(buffIndex), player.Center, Vector2.Zero, ModContent.ProjectileType<FusedExplosion>(), damage, 12f, Main.myPlayer);
+            }
+        }
+        private static bool CanHitPlayer(FakeHeart orig, Player target)
+        {
+            if (orig.Projectile.Colliding(orig.Projectile.Hitbox, target.Hitbox))
+            {
+                if (target.GetModPlayer<FargoSoulsPlayer>().DevianttHeartItem == null)
+                {
+                    target.hurtCooldowns[0] = 0;
+                    int defense = target.statDefense;
+                    float endurance = target.endurance;
+                    target.statDefense = 0;
+                    target.endurance = 0;
+                    target.Hurt(PlayerDeathReason.ByCustomReason(target.name + "感到了心碎。"), orig.Projectile.damage, 0, false, false, false, 0);
+                    target.statDefense = defense;
+                    target.endurance = endurance;
+
+                    if (FargoSoulsUtil.BossIsAlive(ref EModeGlobalNPC.deviBoss, ModContent.NPCType<DeviBoss>()))
+                        target.AddBuff(ModContent.BuffType<Lovestruck>(), 240);
+                }
+                else
+                {
+                    target.statLife += 1;
+                    target.HealEffect(1);
+                }
+
+                orig.Projectile.timeLeft = 0;
+            }
+            return false;
+        }
+
         public override void Unload()
         {
+            On.Terraria.Main.DrawInterface_33_MouseText -= Main_DrawInterface_33_MouseText;
+            On.Terraria.UI.Chat.ChatManager.ParseMessage += ChatManager_ParseMessage;
             foreach (ILHook iLHook in ilHooks)
             {
                 if (iLHook is not null)
